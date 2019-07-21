6e44109cc4a7aab952fe1827593b61d8d2e4d033
Andres Felipe Herrera Moreno <aherrera@truora.com>
2019-07-21 18:34:54 -0500

    Add avaliableMovements

--- methods/dfs.go
+++ methods/dfs.go
@@ -1,8 +1,6 @@
 package methods
 
-import "fmt"
-
 // DFS: Depth First Search
 func DFS(mapGame []string, positions []Position) {
-	fmt.Println("test")
+
 }
--- methods/methods.go
+++ methods/methods.go
@@ -5,3 +5,37 @@ type Position struct {
 	Row    int
 	Column int
 }
+
+func availableMovements(playerPosition Position, mapGame []string) []Position {
+	moves := make([]Position, 0)
+
+	row := playerPosition.Row
+	column := playerPosition.Column
+
+	if column+1 < len(mapGame[row]) && mapGame[row][column+1:] != "W" {
+		moves = append(moves, Position{
+			Row:    row,
+			Column: column + 1,
+		})
+	}
+	if column-1 >= 0 && mapGame[row][column-1:] != "W" {
+		moves = append(moves, Position{
+			Row:    row,
+			Column: column - 1,
+		})
+	}
+
+	if row+1 < len(mapGame) && mapGame[row+1][column:] != "W" {
+		moves = append(moves, Position{
+			Row:    row + 1,
+			Column: column,
+		})
+	}
+	if row-1 >= 0 && mapGame[row-1][column:] != "W" {
+		moves = append(moves, Position{
+			Row:    row - 1,
+			Column: column,
+		})
+	}
+	return moves
+}
