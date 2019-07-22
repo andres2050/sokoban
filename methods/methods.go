94de205eb3b16b92d46675d20b5455d9872341a5
Andres Felipe Herrera Moreno <aherrera@truora.com>
2019-07-21 19:05:14 -0500

    Add registration of movements

--- methods/methods.go
+++ methods/methods.go
@@ -2,8 +2,9 @@ package methods
 
 // Position struct for box location
 type Position struct {
-	Row    int
-	Column int
+	Row       int
+	Column    int
+	Movements string
 }
 
 func availableMovements(playerPosition Position, mapGame []string) []Position {
@@ -11,30 +12,35 @@ func availableMovements(playerPosition Position, mapGame []string) []Position {
 
 	row := playerPosition.Row
 	column := playerPosition.Column
+	movements := playerPosition.Movements
 
 	if column+1 < len(mapGame[row]) && mapGame[row][column+1:] != "W" {
 		moves = append(moves, Position{
-			Row:    row,
-			Column: column + 1,
+			Row:       row,
+			Column:    column + 1,
+			Movements: movements + "R",
 		})
 	}
 	if column-1 >= 0 && mapGame[row][column-1:] != "W" {
 		moves = append(moves, Position{
-			Row:    row,
-			Column: column - 1,
+			Row:       row,
+			Column:    column - 1,
+			Movements: movements + "L",
 		})
 	}
 
 	if row+1 < len(mapGame) && mapGame[row+1][column:] != "W" {
 		moves = append(moves, Position{
-			Row:    row + 1,
-			Column: column,
+			Row:       row + 1,
+			Column:    column,
+			Movements: movements + "D",
 		})
 	}
 	if row-1 >= 0 && mapGame[row-1][column:] != "W" {
 		moves = append(moves, Position{
-			Row:    row - 1,
-			Column: column,
+			Row:       row - 1,
+			Column:    column,
+			Movements: movements + "U",
 		})
 	}
 	return moves
